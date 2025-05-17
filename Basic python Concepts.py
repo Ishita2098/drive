d705792d533998b549479c54a6b7c625915379cd
Ishita2098 <166470418+Ishita2098@users.noreply.github.com>
2025-05-17 13:11:22 +0530

    Add files via upload

---
+++ Basic python Concepts.py
@@ -0,0 +1,10 @@
+A=int(input("Enter the first number :"))
+B=int(input("Enter the second number :"))
+Addition=A+B
+Subtraction=A-B
+Multiplication=A*B
+Division=A/B
+print("Addition",Addition)
+print("Subtraction",Subtraction)
+print("Multiplication",Multiplication)
+print("Division",Division)
---
+++ Basic python concepts2.py
@@ -0,0 +1,4 @@
+X=input("Enter your frist name : ")
+Y=input("Enter your second name : ")
+Z=(X+" "+Y)
+print("Hello," + Z + "! Wellcom to the python program.")
